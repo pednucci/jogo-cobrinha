2d1b954ec098c0c208f6f4046f37647150f308a1
pednucci <pedropetenucci@gmail.com>
2021-03-10 23:51:29 -0300

    erro de entrar na parede corrigido

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ function drawFood() {
 document.addEventListener('keydown', update);
 
 function update (event) {
-    if(event.keyCode == 37 || event.keyCode == 65) direction = "left";
-    if(event.keyCode == 39 || event.keyCode == 68) direction = "right";
-    if(event.keyCode == 38 || event.keyCode == 87) direction = "up";
-    if(event.keyCode == 40 || event.keyCode == 83) direction = "down";
+    if((event.keyCode == 37 || event.keyCode == 65)  && direction != "right") direction = "left";
+    if((event.keyCode == 39 || event.keyCode == 68) && direction != "left") direction = "right";
+    if((event.keyCode == 38 || event.keyCode == 87) && direction != "down") direction = "up";
+    if((event.keyCode == 40 || event.keyCode == 83) && direction != "up") direction = "down";
 }
 
 function iniciarJogo() {
